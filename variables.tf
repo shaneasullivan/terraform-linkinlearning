f93d2eb0776dc15fb1ed91ad23aad827950a48ab
shaneasullivan <shaneasullivan@yahoo.com>
2018-09-26 13:02:13 -0700

    post VPC and subnet terraform

---
+++ variables.tf
@@ -0,0 +1,16 @@
+##################################################################################
+# VARIABLES
+##################################################################################
+
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "aws_role_name" {}
+variable "aws_trusted_account" {}
+variable "aws_region" {}
+variable "environment_tag" {}
+variable "billing_code_tag" {}
+
+#variable "private_key_path" {}
+#variable "key_name" {
+#  default = "RobinKeyPair_AMI"
+#}
